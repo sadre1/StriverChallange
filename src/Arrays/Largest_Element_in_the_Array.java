958975e5bcf7f75dd64019a52934cd753ab5f054
Sadrealam Ahmed <fashel3939@gmail.com>
2023-06-12 23:22:54 +0530

    Second Largest and Second Smallest

--- src/Arrays/Largest_Element_in_the_Array.java
+++ src/Arrays/Largest_Element_in_the_Array.java
@@ -2,14 +2,33 @@ package Arrays;
 
 public class Largest_Element_in_the_Array {
 
-    public int LargestElement(int arr[]){
-        int max= Integer.MIN_VALUE;
+    public int[] LargestElement(int[] a){
+        int n =a.length;
+        int max=a[0];
+        int secondmax=Integer.MIN_VALUE;
+        int min=a[0];
+        int secondmin=Integer.MAX_VALUE;
 
-        for(int x:arr){
-            if(max<x)
-                max=x;
+        for(int i=1;i<n;i++){
+            if(max < a[i]){
+                secondmax=max;
+                max=a[i];
+            }
+            else if(a[i]<max && a[i]>secondmax)
+            {
+                secondmax=a[i];
+            }
+
+            if(min>a[i]){
+                secondmin=min;
+                min=a[i];
+            }
+            else if(a[i]>min && a[i]<secondmin){
+                secondmin=a[i];
+            }
         }
-        return max;
 
+        return new int[]{secondmax,secondmin};
     }
+
 }
