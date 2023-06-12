0bfcd50f466eff20c67fb113d8c635e93adf9fe3
Sadrealam Ahmed <fashel3939@gmail.com>
2023-06-12 23:38:57 +0530

    LinearSearchArray

---
+++ src/Arrays/LinearSearchArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class LinearSearchArray {
+
+
+    public static int linearSearch(int n, int num, int []arr){
+        // Write your code here.
+        for(int i=0;i<n;i++){
+            if(arr[i] == num ){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
